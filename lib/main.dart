f25824ee74432a62b0122b059fbd096108138328
israelemanuel <52048249+israelemanuel@users.noreply.github.com>
2022-07-30 13:27:07 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+            "Pode Entrar",
+            style: TextStyle(
+              fontSize: 26,
+              color: Colors.purple,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text("0")
+        ],
+      ),
+    );
+  }
+}
